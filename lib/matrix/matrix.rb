a4ab3c53f2a6a65dcf17dec34112762f3b7ad573
Alex223124 <sologoubalex@gmail.com>
2019-12-12 21:15:38 +0300

    #color_pixel, #find_pixel, #pixel_exists? methods added

--- lib/matrix/matrix.rb
+++ lib/matrix/matrix.rb
@@ -10,9 +10,22 @@ module Matrix
       @pixels = create_pixels
     end
 
+    def color_pixel(row, column, color)
+      pixel = find_pixel(row, column)
+      pixel.set_color(color)
+    end
+
     def clear_pixels
       service = Pixels::Services::Clear.new(@pixels)
       @pixels = service.run
+    end
+
+    def find_pixel(row, column)
+      @pixels.select{ |p| p.column_num == column.to_i && p.row_num == row.to_i }.first
+    end
+
+    def pixel_exists?(row, column)
+      find_pixel(row, column)
     end
 
     private
