855d210915c17451fffbf0907b481fa1133bd331
Alex223124 <sologoubalex@gmail.com>
2019-12-14 00:55:00 +0300

    methods mapping updated for horizontal and vertical lines, method #run refactored

--- lib/pixels/services/draw_line.rb
+++ lib/pixels/services/draw_line.rb
@@ -2,15 +2,15 @@ module Pixels
   module Services
     class DrawLine
 
-      LINES_MAPPING = {
-          "vertical": "by_column",
-          "horizontal": "by_row"
+      METHODS_MAPPING = {
+          "vertical": {find_by: "by_column", filter_by: "by_row"},
+          "horizontal": {find_by: "by_row", filter_by: "by_column"}
       }
 
-      def initialize(type, pixels, column, starts_at, ends_at, color)
+      def initialize(type, pixels, row_or_column, starts_at, ends_at, color)
         @type = type
         @pixels = pixels
-        @column = column
+        @row_or_column = row_or_column
         @starts_at = starts_at
         @ends_at = ends_at
         @color = color
@@ -25,13 +25,13 @@ module Pixels
       private
 
       def find_whole_line
-        Pixels::Services::Find.new("pixels_#{LINES_MAPPING[@type.to_sym]}",
-                                   @pixels, {attr: @column}).run
+        Pixels::Services::Find.new("pixels_#{METHODS_MAPPING[@type.to_sym][:find_by]}",
+                                   @pixels, {attr: @row_or_column}).run
       end
 
       def filter_by_coordinates(pixels)
-        Pixels::Services::Filter.new("#{LINES_MAPPING[@type.to_sym]}", pixels,
-                                              @starts_at, @ends_at).run
+        Pixels::Services::Filter.new("#{METHODS_MAPPING[@type.to_sym][:filter_by]}",
+                                     pixels, @starts_at, @ends_at).run
       end
 
       def color(pixels)
