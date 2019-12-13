e63520fba8ad1fadf930120fd6c1a7619c7da9b5
Alex223124 <sologoubalex@gmail.com>
2019-12-13 04:38:51 +0300

    new boolean methods added, old methods refactored to single services for scalability and readability

--- lib/matrix/matrix.rb
+++ lib/matrix/matrix.rb
@@ -10,22 +10,43 @@ module Matrix
       @pixels = create_pixels
     end
 
-    def color_pixel(row, column, color)
-      pixel = find_pixel(row, column)
-      pixel.set_color(color)
-    end
-
-    def clear_pixels
-      service = Pixels::Services::Clear.new(@pixels)
-      @pixels = service.run
-    end
-
-    def find_pixel(row, column)
-      @pixels.select{ |p| p.column_num == column.to_i && p.row_num == row.to_i }.first
+    def column_exists?(column)
+      find_pixels_by_column(column).count > 0
     end
 
     def pixel_exists?(row, column)
-      find_pixel(row, column)
+      Pixels::Services::Find.new("pixel", @pixels, {row: row, column: column} ).run
+    end
+
+    def vertical_line_exists?(column, starts_at, ends_at)
+      pixels_by_column = find_pixels_by_column(column)
+      coords = possible_line_coords(starts_at, ends_at)
+      expected_vert_pixels_amount = coords.count
+      result = Pixels::Services::DetectVerticalLine.new(pixels_by_column, coords).run
+      expected_vert_pixels_amount == result.count
+    end
+
+    def color_pixel(row, column, color)
+      Pixels::Services::Color.new("pixel", @pixels, color,
+                                 { row: row, column: column }).run
+    end
+
+    def draw_vertical_line(column, starts_at, ends_at, color)
+      Pixels::Services::DrawVerticalLine.new(@pixels, column, starts_at,
+                                             ends_at, color).run
+    end
+
+    def clear_pixels
+      Pixels::Services::Clear.new(@pixels).run
+    end
+
+    def find_pixels_by_column(column)
+      Pixels::Services::Find.new("pixels_by_column",
+                                 @pixels, {column: column}).run
+    end
+
+    def possible_line_coords(starts_at, ends_at)
+      (starts_at.to_i..ends_at.to_i)
     end
 
     private
